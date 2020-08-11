ae94ace54875807b9782da337c71d99a14c064fb
atulya22 <atulya.shetty@hotmail.com>
2020-08-11 11:41:37 -0400

    SwiftUI Navigation

---
+++ SwiftUINavigation/SwiftUINavigation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUINavigation
+//
+//  Created by Atulya Shetty on 8/11/20.
+//  Copyright © 2020 Atulya Shetty. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
