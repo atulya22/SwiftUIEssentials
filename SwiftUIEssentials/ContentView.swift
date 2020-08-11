bdfd7c485431d7f5765cc3e97721561704ab5414
atulya22 <atulya.shetty@hotmail.com>
2020-08-11 11:22:04 -0400

    Basics of Swift UI combine views

---
+++ SwiftUIEssentials/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  SwiftUIEssentials
+//
+//  Created by Atulya Shetty on 7/22/20.
+//  Copyright © 2020 Atulya Shetty. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+
+            MapView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y:-130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park").font(.subheadline)
+                    Spacer()
+                    Text("California").font(.subheadline)
+                }
+            }
+            .padding()
+            
+            Spacer()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIEssentials/MapView.swift
@@ -0,0 +1,30 @@
+//
+//  MapView.swift
+//  SwiftUIEssentials
+//
+//  Created by Atulya Shetty on 7/22/20.
+//  Copyright © 2020 Atulya Shetty. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        let coordinate = CLLocationCoordinate2D(latitude: 34.011286, longitude: -116.166868)
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        uiView.setRegion(region, animated: true)
+    }
+    
+    
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
